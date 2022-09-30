4315c4d1dcd539b8257d6accc4a825026a426891
habulCoderJohnny <fokruljohnny@gmail.com>
2022-09-30 23:06:14 +0600

    connected mongodb to server and get inserted data

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
+//CONNECT TO MONGODB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.117wic5.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected!!');
+        const servicesCollection = client.db("luxury-living").collection("services");
+
+
+        //GET myOWN Inserted DATA from MongoDB
+        app.get('/services', async (req, res) => {
+        const query = {};
+        const cursor = servicesCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+      });
+}
+
+finally {
+
+}
+
+
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Hello World from luxury-living developing server')
   })
